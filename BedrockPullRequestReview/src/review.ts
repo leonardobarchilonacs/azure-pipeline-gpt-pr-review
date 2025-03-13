d1b1baf9999d779a6a27d9cea815df6f802d5a9f
Leonardo Barchilon <leonardo.barchilon@aquarelacs.com.br>
2025-03-13 10:12:26 -0300

    Alteracao de referencias e cofnig de extension

--- BedrockPullRequestReview/src/review.ts
+++ BedrockPullRequestReview/src/review.ts
@@ -1,3 +1,4 @@
+import fetch from 'node-fetch';
 import { BedrockRuntimeClient, InvokeModelCommand } from '@aws-sdk/client-bedrock-runtime';
 import { git } from './git';
 import { addCommentToPR } from './pr';
